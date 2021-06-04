e820947db4de0b5f6c7928075ac8a2a6f44e7043
priyanka-kaustubh <61750985+priyanka-kaustubh@users.noreply.github.com>
2021-06-04 23:28:06 +0530

    second commit

--- lib/screens/bestDeal_screen.dart
+++ lib/screens/bestDeal_screen.dart
@@ -10,10 +10,41 @@ class _BestDealPageState extends State<BestDealPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          
-        ],
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+            SizedBox(
+              height: 20.0,
+            ),
+
+            Container(
+              margin: EdgeInsets.only(left: 15.0),
+              child: Text(
+                "Included",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w700,
+
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 15.0),
+              child: Text(
+                "For more details press on the icons.",
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 10.0,
+                  color: Colors.grey[500],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
